daa0168ade45c44d79d3422f7062ed5de687a2f8
Welby Seely <welbyseely@gmail.com>
2024-01-12 21:59:29 -0500

    Renamed submodules

---
+++ chat-service/src/main/java/net/jllama/examples/chat/infrastructure/llama2/chat/LlamaEvaluator.java
@@ -0,0 +1,19 @@
+package net.jllama.examples.chat.infrastructure.llama2.chat;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import net.jllama.api.Context;
+import net.jllama.api.Model;
+
+public class LlamaEvaluator {
+
+//  final List<Context> contextPool;
+
+  public LlamaEvaluator(final Model model, final int contextPoolSize) {
+//    IntStream.range(0, contextPoolSize)
+//        .mapToObj(i -> {
+//          return model.newContext()
+//        })
+  }
+
+}
