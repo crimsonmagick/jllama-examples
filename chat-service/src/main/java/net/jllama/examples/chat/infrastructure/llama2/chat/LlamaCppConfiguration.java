9df7806ead82caefff0f613a72380f7431c6e1af
Welby Seely <welbyseely@gmail.com>
2024-01-14 15:21:15 -0500

    Conversation nominally works, but reloading does not

--- chat-service/src/main/java/net/jllama/examples/chat/infrastructure/llama2/chat/LlamaCppConfiguration.java
+++ chat-service/src/main/java/net/jllama/examples/chat/infrastructure/llama2/chat/LlamaCppConfiguration.java
@@ -1,10 +1,6 @@
 package net.jllama.examples.chat.infrastructure.llama2.chat;
 
-import static net.jllama.api.Context.SequenceType.TOKEN;
-
 import jakarta.annotation.PreDestroy;
-import java.util.concurrent.ThreadLocalRandom;
-import net.jllama.api.Context;
 import net.jllama.api.Llama;
 import net.jllama.api.Model;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,25 +18,6 @@ public class LlamaCppConfiguration {
         .path(modelPath)
         .load();
   }
-
-  @Bean
-  Context llamaApiContext(final Model model) {
-    final int threads = Runtime.getRuntime().availableProcessors() / 2 - 1;
-    final int contextSize = 4096;
-    final Context context = model.newContext()
-        .withDefaults()
-        .evaluationThreads(threads)
-        .batchEvaluationThreads(threads)
-        .maximumBatchSize(contextSize)
-        .contextLength(contextSize)
-        .seed(ThreadLocalRandom.current().nextInt())
-        .create();
-    context.batch().type(TOKEN)
-        .configure()
-        .batchSize(contextSize);
-    return context;
-  }
-
   @PreDestroy
   void llamaFree() {
     Llama.library().close();
