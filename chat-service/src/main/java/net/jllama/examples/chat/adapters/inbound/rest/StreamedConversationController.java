ddac80a1014c666e4c3e99147144538772d90049
Welby Seely <welbyseely@gmail.com>
2024-01-20 18:27:19 -0500

    Updated root path to exclude "chat" to allow for other subpaths on controllers

--- chat-service/src/main/java/net/jllama/examples/chat/adapters/inbound/rest/StreamedConversationController.java
+++ chat-service/src/main/java/net/jllama/examples/chat/adapters/inbound/rest/StreamedConversationController.java
@@ -8,10 +8,12 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/chat")
 public class StreamedConversationController {
 
   private static final Logger log = LogManager.getLogger(StreamedConversationController.class);
